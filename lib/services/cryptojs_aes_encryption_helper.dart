75e5aa46795a2d2a6481337b8ab1cf16b33872f8
soesan2020 <soesan2020mgk@gmail.com>
2020-11-21 14:47:11 +0630

    added functions in shop transfer and reset password screen

--- lib/services/cryptojs_aes_encryption_helper.dart
+++ lib/services/cryptojs_aes_encryption_helper.dart
@@ -12,7 +12,6 @@ String encryptAESCryptoJS(String plainText, String passphrase) {
     var keyndIV = deriveKeyAndIV(passphrase, salt);
     final key = encrypt.Key(keyndIV.item1);
     final iv = encrypt.IV(keyndIV.item2);
-
     final encrypter = encrypt.Encrypter(
         encrypt.AES(key, mode: encrypt.AESMode.cbc, padding: "PKCS7"));
     final encrypted = encrypter.encrypt(plainText, iv: iv);
