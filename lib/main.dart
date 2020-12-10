08c0b2d1e5431b61fc5824218a9a15513f16c851
nilarhtun990 <nilarhtun990@gmail.com>
2020-12-10 11:14:54 +0630

    Stock_Ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:retailer/screens/user/login.dart';
 import 'package:retailer/services/functional_provider.dart';
+import 'Stock_ui.dart';
 import 'style/theme.dart' as Style;
 
 void main() {
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Login(),
+      home: TestMainScreen(),
     );
   }
 }
